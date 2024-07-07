f87b8d27e81208c13b8169a730cd1e27215cf5ad
Yenot52 <158297903+Yenot52@users.noreply.github.com>
2024-07-07 14:18:07 +0000

    add exercise 5

---
+++ Exercise 5.py
@@ -0,0 +1,11 @@
+#i
+fruits = ["apples", "grapes", "bananas", "strawberries"]
+
+#ii
+for fruit in fruits:
+    print(fruit)
+
+#iii
+chosen_fruit = fruits[3]
+for letter in chosen_fruit:
+    print(letter)
