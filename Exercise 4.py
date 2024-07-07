3e851ed8b1ed99173010879b58b69a8ce55fa0ac
Yenot52 <158297903+Yenot52@users.noreply.github.com>
2024-07-07 14:29:44 +0000

    add exercise 4

---
+++ Exercise 4.py
@@ -0,0 +1,17 @@
+#i
+x = 5
+
+#ii
+if x == 5:
+    print("This is true")
+else:
+    print("This is false")
+
+#iii
+light = "green"
+if light == "green":
+    print("Pass")
+elif light == "orange":
+    print("Brake")
+elif light == "red":
+    print("Stop")
